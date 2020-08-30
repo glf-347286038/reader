355269f92404c0b5520e8f15467f17256d4a1922
glf-347286038 <491550934@qq.com>
2020-08-30 12:28:49 +0800

    配置类

---
+++ src/main/java/com/reader/common/configuration/CrossOriginConfig.java
@@ -0,0 +1,24 @@
+package com.reader.common.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+/*
+ *AJAX请求跨域
+ * @Author ${USER}
+ * @Date ${DATE} ${TIME}
+ */
+@Configuration
+public class CrossOriginConfig implements WebMvcConfigurer {
+    static final String ORIGINS[] = new String[] {"GET","POST","PUT","DELETE"};
+    public void addCrosMapping(CorsRegistry registry){
+        registry.addMapping("/**")
+                //.allowedOrigins("http://localhost:8085")
+                .allowedOrigins("*")  //支持的域
+                .allowedHeaders("*")
+//                .allowedMethods("*")
+                .allowedMethods(ORIGINS)
+                .allowCredentials(true)
+                .maxAge(3600);   //请求的有效期
+    }
+}
