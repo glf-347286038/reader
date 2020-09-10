6e687b26ef356999d9ec2db250dbbb72bb01d444
glf-347286038 <491550934@qq.com>
2020-09-10 20:08:48 +0800

    配置tomcatHttp请求的字符合法问题

--- src/main/java/com/reader/common/configuration/TomcatConfig.java
+++ src/main/java/com/reader/common/configuration/TomcatConfig.java
@@ -11,7 +11,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class TomcatConf {
+public class TomcatConfig {
     @Bean
     public ConfigurableServletWebServerFactory webServerFactory() {
         TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
